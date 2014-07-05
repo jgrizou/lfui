d29e7a24bfa13bdd7c37d1ae80ec39f6e145c8c4
Jonathan Grizou <jonathan.grizou@inria.fr>
2014-07-05 16:19:11 +0200

    number of iterations between confidence and target reached, added to our high-level analysis

--- highlevel_tools/analysis/recorder_compute_reachingStat.m
+++ highlevel_tools/analysis/recorder_compute_reachingStat.m
@@ -26,6 +26,28 @@ targetCorrect(1:length(tc)) = tc';
 ratioReach = mean(targetCorrect(1:sum(totalReach)));
 ratioReach(isnan(ratioReach)) = 0;
 
+
+confidence = rec.isConfident';
+confidenceSum = totalReach + confidence;
+
+%find time between conf and reach
+reachID = find(confidenceSum == 2);
+
+confReachTimePerTarget = ones(1, maxLength) * -1;
+for iReach = 1:length(reachID)
+    cnt = 0;
+    for i = reachID(iReach)-1:-1:1
+        if confidenceSum(i) > 0
+            cnt = cnt + 1;
+        else
+            break
+        end
+    end
+    confReachTimePerTarget(iReach) = cnt;
+end
+
+
+
 %%
 reachingStat = struct;
 
@@ -38,4 +60,8 @@ reachingStat.timeTarget = timeTarget;
 reachingStat.timePerTarget = timePerTarget;
 reachingStat.targetCorrect = targetCorrect;
 reachingStat.ratioReach = ratioReach;
+
+reachingStat.confidence = confidence;
+reachingStat.confidenceSum = confidenceSum;
+reachingStat.confReachTimePerTarget = confReachTimePerTarget;
 
